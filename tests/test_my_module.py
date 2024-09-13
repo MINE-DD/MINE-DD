ad30ebc978d8dd3259b9d23a05c64deaff96e766
Laura Ootes <l.ootes@esciencecenter.nl>
2024-09-13 12:06:46 +0200

    comment out template tests

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,15 +1,15 @@
 """Tests for the mine_dd.my_module module."""
 import pytest
-from mine_dd.my_module import hello
+"""from mine_dd.my_module import hello
 
 
 def test_hello():
-    """Example using assert."""
+    # Example using assert.
     assert hello('nlesc') == 'Hello nlesc!'
 
 
 def test_hello_with_error():
-    """Example of testing for raised errors."""
+    # Example of testing for raised errors.
     with pytest.raises(ValueError) as excinfo:
         hello('nobody')
     assert 'Can not say hello to nobody' in str(excinfo.value)
@@ -17,10 +17,10 @@ def test_hello_with_error():
 
 @pytest.fixture
 def some_name():
-    """Example fixture."""
+    # Example fixture.
     return 'Jane Smith'
 
 
 def test_hello_with_fixture(some_name):
-    """Example using a fixture."""
-    assert hello(some_name) == 'Hello Jane Smith!'
+    # Example using a fixture.
+    assert hello(some_name) == 'Hello Jane Smith!'"""
