c0a45fc708a4e2538c1a99a09da3611ad319ff08
N400peanuts <n400peanuts@gmail.com>
2025-03-31 14:18:22 +0200

    Restructure package: Replace mine_dd with minedd package structure following Python standards

---
+++ minedd/__init__.py
@@ -0,0 +1,8 @@
+"""
+Mine-DD: Mining the past to protect against diarrheal disease in the future.
+
+This package leverages PaperQA to mine scientific literature for associations between
+climate-sensitive variables and pathogen-specific diarrheal disease incidence.
+"""
+
+__version__ = "0.1.0"
