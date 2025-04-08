7e39851a85517927c153fc9a92907f3dffe4702b
Angel Daza <j.daza@esciencecenter.nl>
2025-04-08 10:37:47 +0200

    comment local test

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -1,10 +1,10 @@
 """Tests for the minedd.embeddings module."""
 import pytest
-import os
-import json
+# import os
+# import json
 import shutil
 from minedd.embeddings import Embeddings
-from paperqa.settings import Settings, AgentSettings
+# from paperqa.settings import Settings, AgentSettings
 
 @pytest.fixture()
 def embeddings(tmp_path):
@@ -23,32 +23,32 @@ def test_prepare_papers(embeddings):
     assert ordered_files == ["nihms-620915.pdf"]
 
 
-def test_process_papers(embeddings):
-    """Test the process_papers method."""
-    local_llm_config = json.load(
-        open("minedd/settings_paperqa.json")
-    )
-    model = local_llm_config["model_list"][0]["model_name"]
-    test_papers_dir = "tests/mock_papers"
+# def test_process_papers(embeddings):
+#     """Test the process_papers method."""
+#     local_llm_config = json.load(
+#         open("minedd/settings_paperqa.json")
+#     )
+#     model = local_llm_config["model_list"][0]["model_name"]
+#     test_papers_dir = "tests/mock_papers"
 
-    settings = Settings(
-        llm=model,
-        llm_config=local_llm_config,
-        summary_llm=model,
-        summary_llm_config=local_llm_config,
-        paper_directory=test_papers_dir,
-        embedding="snowflake-arctic-embed:22m",
-        agent=AgentSettings(
-            agent_llm=model,
-            agent_llm_config=local_llm_config,
-            return_paper_metadata=True
-        )
-    )
-    embeddings.process_papers(settings, test_papers_dir)
+#     settings = Settings(
+#         llm=model,
+#         llm_config=local_llm_config,
+#         summary_llm=model,
+#         summary_llm_config=local_llm_config,
+#         paper_directory=test_papers_dir,
+#         embedding="snowflake-arctic-embed:22m",
+#         agent=AgentSettings(
+#             agent_llm=model,
+#             agent_llm_config=local_llm_config,
+#             return_paper_metadata=True
+#         )
+#     )
+#     embeddings.process_papers(settings, test_papers_dir)
 
-    # Verify embeddings docs are created
-    assert embeddings.docs is not None
+#     # Verify embeddings docs are created
+#     assert embeddings.docs is not None
 
-    # Verify the output file exists
-    assert os.path.exists(embeddings.output_embeddings_path)
+#     # Verify the output file exists
+#     assert os.path.exists(embeddings.output_embeddings_path)
 
