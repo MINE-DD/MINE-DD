130cda4485b3555e92e34e9c2675c93b3301c61c
Angel Daza <j.daza@esciencecenter.nl>
2025-04-11 14:54:04 +0200

    added Embeddings CLI tests

--- minedd/cli.py
+++ minedd/cli.py
@@ -7,15 +7,51 @@ CLI commands for interacting with the package via a terminal
 
 import argparse
 import sys
+import os
 import warnings
 import logging
 
 from minedd.query import Query
+from minedd.embeddings import Embeddings
 
 # suppressing warnings and messages from paper-qa and pydantic
 warnings.filterwarnings("ignore")
 logging.getLogger("litellm").setLevel(logging.ERROR)
 logging.getLogger("paperqa").setLevel(logging.ERROR)
+
+
+def embed_command(args):
+    """ Handle the embed command"""
+
+    emb_path = os.path.join(args.output_dir, args.embeddings_filename)
+
+    # Create the Embeddings object
+    engine = Embeddings(
+        model=args.llm,
+        embedding_model=args.embedding_model,
+        paper_directory=args.paper_directory,
+        output_embeddings_path=emb_path,
+        existing_docs=None
+    )
+
+    # Load existing embeddings if desired and provided
+    if args.augment_existing:
+        if not os.path.exists(emb_path):
+            print(f"Warning: {emb_path} does not exist. Creating from scratch.")
+        else:
+            engine.load_existing_embeddings(emb_path)
+            print(f"Found valid {emb_path} file. Augmenting it...")
+    elif os.path.exists(emb_path):
+            print(f"Warning: {emb_path} already exists. Overwriting it...")
+
+    # Process the papers and create embeddings
+    pdf_file_list = engine.prepare_papers()
+    print(f"Found {len(pdf_file_list)} PDF files in {args.paper_directory}.")
+    if len(pdf_file_list) == 0:
+        print(f"No PDF files found in {args.paper_directory}.")
+        sys.exit(1)
+    else:
+        engine.process_papers(pdf_file_list)
 
 def query_command(args):
     """ Handle the query command"""
@@ -59,10 +95,10 @@ def query_command(args):
         print("Error: Either --questions_file or --question must be provided.")
         sys.exit(1)
 
+
 def main():
+    # Create Main Parser and add common arguments
     parser = argparse.ArgumentParser(description='MINEDD: Query scientific papers with LLMs')
-    
-    # Add query arguments directly to the main parser
     parser.add_argument(
         '--llm',
         type=str,
@@ -75,13 +111,6 @@ def main():
         type=str,
         default='ollama/mxbai-embed-large:latest',
         help='Embedding model to use (default is mxbai-embed-large:latest)'
-    )
-
-    parser.add_argument(
-        '--embeddings',
-        type=str,
-        required=True,
-        help='Path to the embeddings pickle file'
     )
 
     parser.add_argument(
@@ -98,28 +127,67 @@ def main():
         help="Directory to save outputs (default: out)"
     )
 
-    parser.add_argument(
+    # Create subparsers for different commands
+    subparsers = parser.add_subparsers(
+        title='subcommands', 
+        description='Available subcommands: embed | query', 
+        dest='command',
+        required=True
+    )
+    parser_embed = subparsers.add_parser(
+        "embed", 
+        help="Create the paper embeddings in your filesystem"
+    )
+    parser_embed.set_defaults(func=embed_command)
+    parser_query = subparsers.add_parser(
+        "query", 
+        help="Query for existing paper embeddings to obtain answers to questions"
+    )
+    parser_query.set_defaults(func=query_command)
+    
+    # Add arguments specific for the Embed command
+    parser_embed.add_argument(
+        '--embeddings_filename',
+        type=str,
+        default='embeddings.pkl',
+        help='Filename for the embeddings pickle file (default: embeddings.pkl)'
+    )
+    parser_embed.add_argument(
+        '--augment_existing',
+        action='store_true',
+        help='Augment existing embeddings if they exist'
+    )
+
+    # Add arguments specific for the Query command
+    parser_query.add_argument(
+        '--embeddings',
+        type=str,
+        required=True,
+        help='Path to the embeddings pickle file'
+    )
+
+    parser_query.add_argument(
         "--questions_file",
         type=str,
         help="Path to Excel file with questions"
     )
 
-    parser.add_argument(
+    parser_query.add_argument(
         "--question",
         type=str,
         help="Single question to ask"
     )
 
-    parser.add_argument(
+    parser_query.add_argument(
         "--max_retries",
         type=int,
         default=2,
         help="Maximum number of retries for model loading (default: 2)"
     )
 
-    # Parse arguments and call the query command directly
+    # Parse arguments and call the corresponding command
     args = parser.parse_args()
-    query_command(args)
+    args.func(args)
 
 if __name__ == "__main__":
     main()
