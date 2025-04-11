0b719c9fd31cce4fa0ac3c45b74f6b57ef0ed256
Angel Daza <j.daza@esciencecenter.nl>
2025-04-11 10:09:23 +0200

    integrate into general module style

--- minedd/embeddings.py
+++ minedd/embeddings.py
@@ -4,7 +4,7 @@ from difflib import get_close_matches
 from tqdm import tqdm
 from paperqa import Docs
 import pickle as pkl
-from pathlib import Path
+from minedd.utils import configure_settings
 
 
 class Embeddings:
@@ -13,20 +13,33 @@ class Embeddings:
     Each Doc object contains internally the embeddings of chunks of a single paper plus metadata with provenance (n_docs == n_processed_papers).
 
     Attributes:
+        model (str): Generative Model that later will be used for answering queries.
+        embedding_model (str): Encoder Model to use for creating the embeddings.
         output_embeddings_path (str): Path to save the output embeddings file.
+        paper_directory (str): Directory containing the papers to process.
         docs (Docs, optional): Existing Docs object, to be able to manipulate it at Runtime.
     """
     def __init__(self,
-                 output_embeddings_path: str = "embeddings.pkl",
+                 model: str = "ollama/llama3.2:1b",
+                 embedding_model: str = "ollama/mxbai-embed-large:latest",
+                 output_embeddings_path: str = "out/embeddings.pkl",
+                 paper_directory: str = "data/",
                  existing_docs: Docs = None,
                  ):
         self.output_embeddings_path = output_embeddings_path
+        self.paper_directory = paper_directory
+        self.embedding_model = embedding_model
         self.docs = existing_docs
+
+        self.settings = configure_settings(model, self.embedding_model, self.paper_directory)
+
+        emb_directory = os.path.dirname(self.output_embeddings_path)
+        if emb_directory:
+            os.makedirs(emb_directory, exist_ok=True)
 
 
     def __repr__(self):
-        #TODO: If possible show the Dimensions of the embeddings table [n_docs, dimensions]
-        return f"Embeddings(output_embeddings_path={self.output_embeddings_path}, docs={self.docs})"
+        return f"Embeddings(output_embeddings_path={self.output_embeddings_path}, docs={len(self.docs)})"
 
     def load_existing_embeddings(self, embeddings_path: str):
         """Load existing embeddings (List of Docs) from the specified path."""
@@ -84,25 +97,26 @@ class Embeddings:
 
         # Convert to DataFrame for better visualization
         details_df = pd.DataFrame(doc_details)
-        # details_dict = details_df.to_dict(orient='records')
-
         return details_df
 
-    def prepare_papers(self, papers_directory: Path):
+    def prepare_papers(self, paper_directory: str = None):
         """Takes a Path object with the location of PDFs to process and returns a list of the valid papers in the directory.
-
         Args:
-            papers_directory (Path): location of papers in PDF format.
+            paper_directory (str, optional): location of (new) papers in PDF format. If None it takes the default paper directory
 
         Returns:
             ordered_file_list (str): list of the valid paper names in the given directory.
         """
+        # Assign new paper directory if given. Otherswise, use the default one.
+        if paper_directory is not None:
+            self.paper_directory = paper_directory
+        # Check that directory is found and list all files in it
         try:
-            file_list = os.listdir(papers_directory)
+            file_list = os.listdir(self.paper_directory)
         except FileNotFoundError:
-            print(f"WARNING: Directory {papers_directory} not found.")
+            print(f"WARNING: Directory {self.paper_directory} not found.")
             return []
-        path_files = [os.path.join(papers_directory, f) for f in file_list]
+        path_files = [os.path.join(self.paper_directory, f) for f in file_list]
 
         path_df = pd.DataFrame(
             {
@@ -115,46 +129,42 @@ class Embeddings:
         for title in path_df['PDF Name']:
             closest_match = get_close_matches(title, file_list, n=1)
             ordered_file_list.append(closest_match[0] if closest_match else None)
-
         # Drop None values from the ordered list if necessary
         ordered_file_list = [file for file in ordered_file_list if file is not None]
-
         return ordered_file_list
 
-    def process_papers(self, settings,  papers_directory: Path, paper_list: list[str]):
+    def process_papers(self, paper_list: list[str]):
         """Transforms a list of valid PDFs into Doc Embeddings.
-        The PDFs are chunked, processed and the embeddings are created using the provided settings.
+        The PDFs are chunked, processed and the embeddings are created using the Settings.
         The resulting embeddings are saved to the specified output path.
 
         Args:
-            settings (Settings): PaperQA settings object, describing the LLMs hyperparameters.
-            papers_directory (Path): location of directory containing the papers in PDF format. Needed for metadata.
             paper_list (list[str]): List of valid PDFs containing the papers that will be transformed into Embeddings.
         """
-
-        ordered_file_list = paper_list
-
+        
+        # If Embeddings object has docs, we will add the new papers to it.
         if self.docs is not None:
             print("Adding new docs to the existing Embeddings.")
             docs = self.docs
         else:
             print("Creating new Docs object.")
             docs = Docs(index_path=self.output_embeddings_path)
-
-        for i, doc in tqdm(enumerate(ordered_file_list), total=len(ordered_file_list)):
+        
+        # Process each paper and add it to a PaperQA Docs object
+        for i, doc in tqdm(enumerate(paper_list), total=len(paper_list), desc="Processing papers:", unit="paper"):
             try:
                 docs.add(
-                        path=str(os.path.join(papers_directory, doc)),
+                        path=str(os.path.join(self.paper_directory, doc)),
                         docname=doc,
-                        settings=settings
+                        settings=self.settings
                 )
                 print(f"Correctly loaded {doc}")
             except Exception as e:
                 print(f"Could not read {doc}: {e}")
                 continue
-
-        self.docs = docs
+        
         # Save the Docs object
+        self.docs = docs
         with open(self.output_embeddings_path, "wb") as f:
             pkl.dump(docs, f)
         print(f"Docs object saved to {self.output_embeddings_path}")
