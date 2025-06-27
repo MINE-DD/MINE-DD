e08725a6966fac31cf7c5ef05cea678558ef42ca
Angel Daza <j.daza@esciencecenter.nl>
2025-06-27 15:14:14 +0200

    small scale rag implemented

--- minedd/document.py
+++ minedd/document.py
@@ -2,17 +2,9 @@ import re
 import os
 import json
 import pandas as pd
-# For Marker as PDF to MArkdown Converter
-from marker.converters.pdf import PdfConverter
-from marker.models import create_model_dict
-from marker.output import text_from_rendered
-from marker.config.parser import ConfigParser
 # For Grobid-Based PDF Text extraction
 from langchain_community.document_loaders.parsers import GrobidParser
 from langchain_community.document_loaders.generic import GenericLoader
-# For Table Extraction
-from gmft.auto import AutoTableDetector, AutoTableFormatter
-from gmft.pdf_bindings import PyPDFium2Document
 # For Chunking Texts
 from langchain_core.documents import Document
 from langchain_text_splitters.character import CharacterTextSplitter, RecursiveCharacterTextSplitter
@@ -36,18 +28,33 @@ class DocumentPDF:
                 "paginate_output": False # Set to True if you need pagination string separators
             }
         else:
-            # Ensure the model is set correctly in the config
             self.marker_config = marker_config
+        self.marker_converter = None
+        
 
-        # Create config parser
-        config_parser = ConfigParser(self.marker_config)
-        self.marker_converter = PdfConverter(
-            config=config_parser.generate_config_dict(),
-            artifact_dict=create_model_dict(),
-            processor_list=config_parser.get_processors(),
-            renderer=config_parser.get_renderer(),
-            llm_service=config_parser.get_llm_service()
-        )
+    def _init_marker(self):
+        """Initialize Marker only when needed"""
+
+        if self.marker_converter is None:
+            # Import at runtime For Marker as PDF to Markdown Converter
+            from marker.converters.pdf import PdfConverter
+            from marker.models import create_model_dict
+            from marker.output import text_from_rendered
+            from marker.config.parser import ConfigParser
+            try:
+                config_parser = ConfigParser(self.marker_config)
+                self.marker_converter = PdfConverter(
+                    config=config_parser.generate_config_dict(),
+                    artifact_dict=create_model_dict(),
+                    processor_list=config_parser.get_processors(),
+                    renderer=config_parser.get_renderer(),
+                    llm_service=config_parser.get_llm_service()
+                )
+                self.text_from_rendered = text_from_rendered
+                print("Marker initialized successfully.")
+            except Exception as e:
+                print(f"Failed to initialize Marker: {e}")
+                self.marker_converter = None
 
     @classmethod
     def from_json(cls, json_path: str):
@@ -59,18 +66,17 @@ class DocumentPDF:
             return None
         except json.decoder.JSONDecodeError:
             print(f"File {json_path} is empty or not a valid JSON! Returning None")
-            return None
-            
+            return None          
 
-        # try:
-        doc = cls(content["original_path"])
-        doc.markdown = content["markdown"]
-        doc.json_content = content["text_chunks"]
-        doc.tables = [pd.DataFrame(t) for t in content["tables_as_json"]]
-        return doc
-        # except:
-        #     print(f"Problem loading {json_path}. Check that the file has the right format. Returning None")
-        #     return None
+        try:
+            doc = cls(json_path.replace('.json', '.pdf'))  # Assuming the PDF file has the same name as the JSON file
+            doc.markdown = content["markdown"]
+            doc.json_content = content["text_chunks"]
+            doc.tables = [pd.DataFrame(t) for t in content["tables_as_json"]]
+            return doc
+        except:
+            print(f"Problem loading {json_path}. Check that the file has the right format. Returning None")
+            return None
 
 
     def get_markdown(self) -> str:
@@ -80,10 +86,11 @@ class DocumentPDF:
             if self.marker_config.get("output_format") != "markdown":
                 raise ValueError("Output format must be set to 'markdown' in the configuration.")
             try:
+                self._init_marker()  # Initialize only when actually needed
                 # Convert PDF to markdown
                 rendered = self.marker_converter(self.pdf_path)
                 # Extract the markdown text and images
-                marker_text, _, images = text_from_rendered(rendered)
+                marker_text, _, images = self.text_from_rendered(rendered)
                 self.markdown = marker_text
                 return marker_text
             except Exception as e:
@@ -161,6 +168,10 @@ class DocumentPDF:
 
     def get_document_tables(self) -> list[str]:
         "Uses GMT to extract tables as loadable CSV files from the PDF document. Stores the tables in a list of loadable strings."
+        # For Table Extraction
+        from gmft.auto import AutoTableDetector, AutoTableFormatter
+        from gmft.pdf_bindings import PyPDFium2Document
+        
         detector = AutoTableDetector()
         formatter = AutoTableFormatter()
         doc = PyPDFium2Document(self.pdf_path)
@@ -177,19 +188,39 @@ class DocumentPDF:
 
 
     def get_chunks(self, mode:str='chars', chunk_size:int=1000, overlap:int=100, as_langchain_docs:bool=False) -> list[str] | list[Document]:
-        "In case grobid server is not working just use the markdown content to extract chunks"
-        md_doc = DocumentMarkdown(md_content=self.get_markdown())
-        chunks = md_doc.convert_to_chunks(mode='chars', chunk_size=chunk_size, overlap=overlap)
-        if as_langchain_docs:
-            docs = []
-            for i, chunk in enumerate(chunks):
-                docs.append(Document(
-                    page_content= chunk,
-                    metadata={"chunk_index": i, "filepath": self.pdf_path}
-                ))
+        """_summary_
+
+        Args:
+            mode (str, optional): Type of Chunking to be done. Defaults to 'chars'.
+            chunk_size (int, optional): Chunk size normally in characters but if mode is 'from_json' then units are sentences. Defaults to 1000.
+            overlap (int, optional): Chunk overlap normally in characters but if mode is 'from_json' then units are sentences. Defaults to 100.
+            as_langchain_docs (bool, optional): Wether to return already langchain Document objects. Defaults to False.
+
+        Raises:
+            ValueError: Checks if the mode is valid. If not, raises an error.
+
+        Returns:
+            list[str] | list[Document]: List of text chunks or LangChain Document objects depending on the `as_langchain_docs` flag.
+        """
+        if mode not in ['chars', 'from_json']:
+            raise ValueError("Chunk extraction NOT supported! Mode must be 'chars' (from Markdown) or 'from_json' (From Grobid Generated JSON).")
+        if mode == 'from_json':
+            filename = os.path.basename(self.pdf_path)
+            docs = process_from_grobid_chunks(filename, self.json_content, as_langchain_docs=as_langchain_docs, chunk_size=chunk_size, overlap=overlap)
             return docs
         else:
-            return chunks
+            md_doc = DocumentMarkdown(md_content=self.get_markdown())
+            chunks = md_doc.convert_to_chunks(mode='chars', chunk_size=chunk_size, overlap=overlap)
+            if as_langchain_docs:
+                docs = []
+                for i, chunk in enumerate(chunks):
+                    docs.append(Document(
+                        page_content= chunk,
+                        metadata={"chunk_index": i, "filepath": self.pdf_path}
+                    ))
+                return docs
+            else:
+                return chunks
     
     def get_document_as_dict(self):
         return {
@@ -202,6 +233,50 @@ class DocumentPDF:
     def to_json(self, json_path: str):
         with open(json_path, 'w', encoding='utf-8') as json_file:
             json.dump(self.get_document_as_dict(), json_file, indent=2)
+
+
+
+def process_from_grobid_chunks(filename, json_content, as_langchain_docs:bool=False, chunk_size:int=1000, overlap:int=100):
+
+    def _process_json_batch(batch):
+        if as_langchain_docs:
+            text = " ".join([c['text'] for c in batch])
+            metadata = {
+                "source": filename,
+                "title": doc_title,
+                "pages": f"{batch[0]['pages'].split('-')[0]}-{batch[-1]['pages'].split('-')[-1]}",
+                "chunk_index": f"{batch[0]['chunk_index']}-{batch[-1]['chunk_index']}",
+                "section": batch[0]['section'],
+                "sentence_count": len(batch),
+            }
+            doc = Document(
+                page_content=text,
+                metadata=metadata
+            )
+            return doc
+        else:
+            return [c['text'] for c in batch]
+    # json_content = self.json_content
+    if json_content is None:
+        raise ValueError("No JSON content available. Please run 'get_grobid_chunks' first.")
+    print("Processing JSON content into chunks of size ", chunk_size, " sentences with overlap ", overlap)
+    doc_title = json_content['title']
+    is_grouped_by_section = json_content['grouped_by_section']
+    chunks_key = 'sections_content' if is_grouped_by_section else 'chunks'
+    step_size = max(1, chunk_size - overlap)  # Ensure step_size >= 1
+    documents = []
+    if is_grouped_by_section:
+        for section_name, sentences in json_content[chunks_key].items():
+            for i in range(0, len(sentences), step_size):
+                batch = sentences[i:i + chunk_size]
+                doc = _process_json_batch(batch)
+                documents.append(doc)
+    else:
+        for i in range(0, len(json_content[chunks_key]), step_size):
+            batch = json_content[chunks_key][i:i + chunk_size]
+            doc = _process_json_batch(batch)
+            documents.append(doc)
+    return documents
 
 
 
--- minedd/rag.py
+++ minedd/rag.py
@@ -1,7 +1,7 @@
-# Source: https://python.langchain.com/docs/tutorials/rag/
+# https://python.langchain.com/api_reference/community/vectorstores/langchain_community.vectorstores.faiss.FAISS.html#langchain_community.vectorstores.faiss.FAISS
 # %pip install --quiet --upgrade langchain-text-splitters langchain-community langgraph 
 # pip install -qU "langchain[cohere]"
-# pip install -qU langchain-huggingface
+# pip install -qU langchain-huggingface OR langchain-ollama
 # pip install faiss-cpu
 # pip install cohere
 
@@ -10,41 +10,27 @@ import cohere
 import faiss
 import numpy as np
 import pandas as pd
+from uuid import uuid4
+from rank_bm25 import BM25Okapi
+from sklearn.feature_extraction import _stop_words
+import string
+from tqdm import tqdm
+from pathlib import Path
+import json
+
+from langchain_core.documents import Document
+from langchain_ollama import OllamaEmbeddings
+from langchain.chat_models import init_chat_model
+from langchain_community.vectorstores import FAISS
+from langchain_community.docstore.in_memory import InMemoryDocstore
 from minedd.document import DocumentMarkdown
+from minedd.document import DocumentPDF
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
 
-cohere_api_key = os.getenv('COHERE_API_KEY')
-co = cohere.Client(cohere_api_key)
-
-# llm = init_chat_model("command-r-plus", model_provider="cohere")
-
-# # Embeddings model
-# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
-# embedding_dim = len(embeddings.embed_query("hello world"))
-
-
-# vector_store = FAISS(
-#     embedding_function=embeddings,
-#     index=index,
-#     docstore=InMemoryDocstore(),
-#     index_to_docstore_id={},
-# )
-
-
-
-
-# # Create a Document object for each chunk
-# docs = []
-# for i, chunk in enumerate(chunks):
-#     doc = Document(page_content=chunk, metadata={"source": "paper_text.md", "title": markdown_paper.get_title(), "chunk_index": i})
-#     docs.append(doc)
-
-# # Index chunks
-# vector_store.add_documents(documents=docs)
-
-def create_document_database(filenames):
+# This function is to create a document database from a list of markdown files.
+def documentdb_from_markdown(filenames):
     chunks = []
     # Load Document(s)
     for filename in filenames:
@@ -66,43 +52,258 @@ def create_document_database(filenames):
     return index, chunks
 
 
-
-def search(query, texts_np, index, number_of_results=3):
-
-    # 1. Get the query's embedding
-    query_embed = co.embed(texts=[query],
-                input_type="search_query",).embeddings[0]
-
-    # 2. Retrieve the nearest neighbors
-    distances , similar_item_ids = index.search(np.float32([query_embed]), number_of_results)
-
-    # 3. Format the results
-    results = pd.DataFrame(data={'texts': texts_np[similar_item_ids[0]],
-                                'distance': distances[0]})
-
-    # 4. Print and return the results
-    print(f"Query:'{query}'\nNearest neighbors:")
-    return results
+def bm25_tokenizer(text):
+    tokenized_doc = []
+    for token in text.lower().split():
+        token = token.strip(string.punctuation)
+        if len(token) > 0 and token not in _stop_words.ENGLISH_STOP_WORDS:
+            tokenized_doc.append(token)
+    return tokenized_doc
 
 
+def get_bm25_index(texts):
+    tokenized_corpus = []
+    for passage in tqdm(texts):
+        tokenized_corpus.append(bm25_tokenizer(passage))
+    bm25 = BM25Okapi(tokenized_corpus)
+    return bm25
 
-## Load Docs
-filenames = ["notebooks/outputs/paper_text.md"]
-index, texts = create_document_database(filenames)
-texts_np = np.array(texts) # Convert texts list to numpy for easier indexing
+
+def keyword_and_reranking_search(texts, bm25, query, top_k=3, num_candidates=10):
+    print("Input question:", query)
+
+    ##### BM25 search (lexical search) #####
+    bm25_scores = bm25.get_scores(bm25_tokenizer(query))
+    top_n = np.argpartition(bm25_scores, -num_candidates)[-num_candidates:]
+    bm25_hits = [{'corpus_id': idx, 'score': bm25_scores[idx]} for idx in top_n]
+    bm25_hits = sorted(bm25_hits, key=lambda x: x['score'], reverse=True)
+
+    print(f"Top-3 lexical search (BM25) hits")
+    for hit in bm25_hits[0:top_k]:
+        print("\t{:.3f}\t{}".format(hit['score'], texts[hit['corpus_id']].replace("\n", " ")))
+
+    #Add re-ranking
+    docs = [texts[hit['corpus_id']] for hit in bm25_hits]
+
+    print(f"\nTop-3 hits by rank-API ({len(bm25_hits)} BM25 hits re-ranked)")
+    results = co.rerank(query=query, documents=docs, top_n=top_k, return_documents=True)
+    return results.results
 
 
-query = "How do climate variables affect the spread of rotavirus?"
+class PersistentFAISS:
+    def __init__(self, index_path, index, embeddings_engine, include_bm25=False):
+        self.index_path = index_path
+        self.embeddings = embeddings_engine
+        self.vector_index = index
+        self.vector_store = None
+        self.include_bm25 = include_bm25
+        self.bm25_index = None
+        
+    def initialize(self, documents=None):
+        """Initialize or load the vector store"""
+        if self._index_exists():
+            self._load_existing_index()
+            self._load_bm25_index() if self.include_bm25 else None
+        elif documents is None:
+            print("No existing index found, creating an empty new one...") 
+            self.vector_store = FAISS(
+                embedding_function=self.embeddings,
+                index=self.vector_index,
+                docstore=InMemoryDocstore(),
+                index_to_docstore_id={},
+            )
+        else:
+            print(f"No existing index found, creating a new one with {len(documents)} documents...")    
+            self._create_new_index(documents)
+            self._load_bm25_index() if self.include_bm25 else None
+    
 
-# 1- Retrieval
-# We'll use embedding search. But ideally we'd do hybrid
-results = search(query, texts_np, index)
+    def _index_exists(self):
+        """Check if FAISS index exists"""
+        return os.path.exists(self.index_path) and os.listdir(self.index_path)
+    
+    def _load_existing_index(self):
+        """Load existing FAISS index"""
+        print(f"Loading existing FAISS index from {self.index_path}")
+        self.vector_store = FAISS.load_local(
+            self.index_path,
+            self.embeddings,
+            allow_dangerous_deserialization=True
+        )
+        print("Index loaded successfully")
 
-# 2- Grounded Generation
-docs_dict = [{'text': text} for text in results['texts']]
-response = co.chat(
-    message = query,
-    documents=docs_dict
-)
+    def _load_bm25_index(self):
+        tokenized_corpus = []
+        texts = [doc.page_content for doc in self.get_all_documents()]
+        for passage in tqdm(texts):
+            tokenized_corpus.append(bm25_tokenizer(passage))
+        self.bm25_index = BM25Okapi(tokenized_corpus)
 
-print(response.text)
+    def _create_new_index(self, documents):
+        """Create new FAISS index from documents"""
+        print("Creating new FAISS index...")
+        self.vector_store = FAISS.from_documents(documents, self.embeddings)
+        self._save_index()
+        print("New index created and saved")
+    
+    def _save_index(self):
+        """Save the current index"""
+        os.makedirs(self.index_path, exist_ok=True)
+        self.vector_store.save_local(self.index_path)
+    
+    def get_all_documents(self):
+        """Get all documents from the vector store"""
+        if self.vector_store is None:
+            raise ValueError("Vector store not initialized")
+        
+        return self.vector_store.docstore._dict.values()
+
+    def add_documents(self, documents):
+        """Add new documents to existing index"""
+        if self.vector_store is None:
+            raise ValueError("Vector store not initialized")
+        uuids = [str(uuid4()) for _ in range(len(documents))]
+        self.vector_store.add_documents(documents=documents, ids=uuids)
+        self._save_index()
+        print(f"Added {len(documents)} documents and saved index")
+    
+    def search(self, query, k=3, hybrid=False, num_candidates=10):
+        """Perform similarity search"""
+        if self.vector_store is None:
+            raise ValueError("Vector store not initialized")
+        
+        if hybrid and self.include_bm25:
+            return keyword_and_reranking_search(self.bm25, query, top_k=k, num_candidates=10)
+        else:
+            return self.vector_store.similarity_search(query, k=k)
+
+
+class SimpleRAG:
+    def __init__(self, embeddings, generative_llm, vector_store):
+        self.llm = generative_llm
+        self.embeddings = embeddings
+        self.vector_store = vector_store
+
+    def get_documents_from_directory(self, directory, extensions=['.json'], chunk_size=10, overlap=5):
+        """Pre-process a directory of documents and load them into the vector store"""
+        # Load documents from directory
+        documents = []
+        processed_files = 0
+        for filename in os.listdir(directory):
+            if '.json' in extensions and filename.endswith('.json'):
+                processed_files += 1
+                full_paper = DocumentPDF.from_json(json_path=os.path.join(directory, filename))
+                paper_chunks = full_paper.get_chunks(
+                    mode='from_json', 
+                    chunk_size=chunk_size, 
+                    overlap=overlap, 
+                    as_langchain_docs=True
+                    )
+                documents.extend(paper_chunks)
+
+        print(f"Processed {processed_files} files, extracted {len(documents)} Document chunks")
+        return documents
+
+    def load_documents(self, documents):
+        """Load documents into the vector store"""
+        if self.vector_store is None:
+            raise ValueError("Vector store not initialized")
+        self.vector_store.add_documents(documents)
+
+
+def run_vanilla_rag(embeddings, llm):
+    
+    # Index and Store Embeddings
+    index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world")))
+    vector_store_path = "minedd_test_index"
+    vector_store = PersistentFAISS(
+        index_path=vector_store_path,
+        index=index, 
+        embeddings_engine=embeddings,
+        include_bm25=True
+        )
+    
+    # Create RAG Engine
+    rag_engine = SimpleRAG(
+        embeddings=ollama_embeddings,
+        generative_llm=llm,
+        vector_store=vector_store
+    )
+
+    # ## Load Docs + Create a Document object for each chunk
+    if os.path.exists(vector_store_path) and os.listdir(vector_store_path):
+        print(f"Loading existing vector store from {vector_store_path}")
+        vector_store.initialize()
+    else:
+        print(f"No existing vector store found at {vector_store_path}, Chunking and Loading Documents to create one...")
+        docs = rag_engine.get_documents_from_directory(
+            directory=Path.home() / "papers_minedd",
+            extensions=['.json'],
+            chunk_size=8, # Number of sentences to merge into one Document
+            overlap=2 # Number of sentences to overlap between chunks
+        )
+        vector_store.initialize(documents=docs)
+
+    # 0 - Query Vector Store
+    query = "How is rotavirus related to seasonality? Please also refer to the sources of your claims (according to the context provided to you)"
+
+    # 1- Retrieval
+    # We'll use embedding search. But ideally we'd do hybrid
+    print("\n\n----- FAISS Index Vector Search\n\n")
+    results = vector_store.search(query, k=3)
+
+    for r in results:
+        print(r)
+
+
+    ## 2 - ReRanking (Using Cohere?)
+    print("\n\n----- BM25 + ReRanker\n\n")
+    texts = [doc.page_content for doc in vector_store.get_all_documents()]
+    results_hybrid = keyword_and_reranking_search(texts, vector_store.bm25_index, query, top_k=3, num_candidates=10)
+    for hit in results_hybrid:
+        print("\t{:.3f}\t{}".format(hit.relevance_score, hit.document.text.replace("\n", " ")))
+
+    # 3 - Grounded Generation (Using Cohere?)
+    docs_dict = [{'text': doc.page_content} for doc in results]
+    response = co.chat(
+        message = query,
+        documents=docs_dict
+    )
+    print(f"Response FAISS:\n{response.text}")
+
+
+    docs_dict = [{'text': doc.document.text} for doc in results_hybrid]
+    response = co.chat(
+        message = query,
+        documents=docs_dict
+    )
+    print(f"Response Hybrid ReRanked:\n{response.text}")
+
+
+if __name__ == "__main__":
+    # model_name="command-r-plus",
+    # model_provider="cohere"
+    ### OR
+    # model_name="gemini-2.5-flash-lite-preview-06-17",
+    # model_provider="google_genai"
+    ### OR
+    # model_name="llama3.2:latest"
+    # model_provider="ollama"
+
+    model_name="command-r-plus"
+    model_provider="cohere"
+
+    # For now will use Cohere Re-Ranker
+    cohere_api_key = os.getenv('COHERE_API_KEY')
+    co = cohere.Client(cohere_api_key)
+
+    llm = init_chat_model(model_name, model_provider=model_provider)
+    # llm = init_chat_model("gemini-2.5-flash-lite-preview-06-17", model_provider="google_genai")
+
+    # Embeddings model
+    ollama_embeddings = OllamaEmbeddings(model="mxbai-embed-large:latest")
+
+    run_vanilla_rag(
+        embeddings=ollama_embeddings,
+        llm=llm
+    )
